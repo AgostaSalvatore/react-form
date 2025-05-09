ef63f8472d9e7b288696b6ff0843b5d0c29e1435
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-09 13:24:48 +0200

    [UPD] added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 const listItems = [
-  { id: 1, name: 'Item ' },
+  { id: 1, name: 'Item 1' },
   { id: 2, name: 'Item 2' },
   { id: 3, name: 'Item 3' },
   { id: 4, name: 'Item 4' },
